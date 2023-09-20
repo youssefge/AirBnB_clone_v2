d819b0b80939c89a824b61ff37b1c85c3c638398
youssefge <elboudaliyoussef@gmail.com>
2023-09-20 14:49:34 -0700

    fsfs

--- models/user.py
+++ models/user.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-import os
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    password = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    first_name = Column(
-        String(128), nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    last_name = Column(
-        String(128), nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    places = relationship(
-        'Place',
-        cascade="all, delete, delete-orphan",
-        backref='user'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
-    reviews = relationship(
-        'Review',
-        cascade="all, delete, delete-orphan",
-        backref='user'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+    __tablename__ = "users"
+    email = Column('email', String(128), nullable=False)
+    password = Column('password', String(128), nullable=False)
+    first_name = Column('first_name', String(128), nullable=True, default="NULL")
+    last_name = Column('last_name', String(128), nullable=True, default="NULL")
+    # backref may need to be back_populates?Below line commented out bc console
+    # would not run with it in. This line was implemented in Task 8
+    places = relationship("Place", cascade="delete", backref="user")
+    # Below line is commented out for caution and was added in Task 9
+    reviews = relationship("Review", cascade="delete", backref="user")
